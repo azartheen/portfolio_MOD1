3c0ff5d68c800233d22e60c8b045c4de7570c77a
azartheen <asartheen916@gmail.com>
2021-07-03 08:36:14 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+import { Button } from "@material-ui/core";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch,
+  Redirect,
+} from "react-router-dom";
+import Navbar from "./navbar/Navbar";
+import Home from "./home/Home";
+import Contact from "./contact/Contact";
+import About from "./about/About";
+import Resume from "./resume/Resume";
+
+function App() {
+  return (
+    <div>
+      <div>
+        <Navbar />
+      </div>
+      <div>
+        <Router>
+          <Switch>
+            <Route path="/Contact">
+              <Contact />
+            </Route>
+            <Route path="/About">
+              <About />
+            </Route>
+            <Route path="/Resume">
+              <Resume />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </Router>
+      </div>
+    </div>
+  );
+}
+
+export default App;
